eee3bbdec8607c9da5f017b67cf7c9f875e77d0f
BrodyLawson <43419130+BrodyLawson@users.noreply.github.com>
2018-10-09 13:52:11 -0400

    Added Touch and React

--- MovingImagesBrody/main.lua
+++ MovingImagesBrody/main.lua
@@ -21,3 +21,17 @@ beetleship.alpha = 0
 -- set the initial x and y position of the beetleship
 beetleship.x = 0
 beetleship.y = display.contentHeight/3
+
+-- Function: MoveShip
+-- Input: this function accepts
+-- Output: none
+-- Description: This function adds speed to the x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to the x-value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+	--change the transparency of the ship every time it moves so that it fades in
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveShip) 
--- TouchAndDragBrody/main.lua
+++ TouchAndDragBrody/main.lua
@@ -1,7 +1,71 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndDrag
+-- Name: Brody Lawson
+-- Course: ICS2O
+-- THis Program displays images that react to a perrson's finger.
 
--- Your code here
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- local Variables
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local rocketShip = display.newImageRect("Images/rocketship.png", 200, 200 )
+local star = display.newImageRect("Images/star.png", 200, 200 ) 
+
+--my boolean variables to keep track of which object i touched first
+local alreadyTouchedRocketShip = false
+local alreadyTouchedStar = false
+
+-- set the initial x and y position of my image
+rocketShip.x = 250
+rocketShip.y =150
+
+
+star.x = 350
+star.y = 450
+
+-- Function: RocketShipListener 
+-- Input: touch listener
+-- Output: none
+-- Description: when rocketship is touched, move her
+local function RocketShipListener(touch)
+
+   if (touch.phase == "began") then
+ 	   if (alreadyTouchedRocketShip == false) then
+ 		alreadyTouchedRocketShip = true 
+       end
+    end
+
+    if ( (touch.phase == "moved") and (alreadyTouchedRocketShip == true) ) then
+    	rocketShip.x = touch.x
+    	rocketShip.y = touch.y
+    end
+
+    if (touch.phase == "ended") then
+        alreadyTouchedRocketShip = false
+        alreadyTouchedStar = false
+    end
+end
+
+local function StarListener(touch)
+
+   if (touch.phase == "began") then
+ 	   if (alreadyTouchedStar == false) then
+ 		alreadyTouchedStar = true 
+       end
+    end
+
+    if ( (touch.phase == "moved") and (alreadyTouchedStar == true) ) then
+    	star.x = touch.x
+    	star.y = touch.y
+    end
+
+    if (touch.phase == "ended") then
+        alreadyTouchedStar = false
+        alreadyTouchedRocketShip = false
+    end
+end
+
+-- add the respective listener to each object
+rocketShip:addEventListener("touch", RocketShipListener)
+
+star:addEventListener("touch", StarListener)
